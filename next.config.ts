85a797185536b924aee2da971c08c0a19565c2e3
frankenstein-code <mail2pruthvi22@gmail.com>
2025-07-26 16:00:59 +0530

    initial commit v5

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  compilerOptions: {
-    baseUrl: "src",
-  },
 };
 
 export default nextConfig;
